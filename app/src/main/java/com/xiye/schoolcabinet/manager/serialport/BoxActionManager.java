41906b77743f0a8fa0925a7dc2a162f5f52c12e8
DwyaneTyroneWade <392812811@qq.com>
2016-07-22 11:05:22 +0800

    4*N 箱子开锁
    从<=14 * 4 箱子 优化 到4*N个箱子

--- app/src/main/java/com/xiye/schoolcabinet/manager/serialport/BoxActionManager.java
+++ app/src/main/java/com/xiye/schoolcabinet/manager/serialport/BoxActionManager.java
@@ -125,55 +125,67 @@ public class BoxActionManager {
         return b;
     }
 
+    /**
+     * 4*N
+     *
+     * @param boxId
+     * @param actionType
+     * @return
+     */
     private byte[] obtainCommandForBoxTypeA(String boxId, LockActionType actionType) {
-        //TODO <=14个子柜子，每个子柜4个箱子
         byte[] b = new byte[6];
         int boxIdInt = Integer.valueOf(boxId);
         int subCabinetNo = 0;//子柜号
         int lockNo = 0;//锁号
-        if (boxIdInt <= 4) {
-            subCabinetNo = 1;
-            lockNo = boxIdInt - 4 * 0;
-        } else if (boxIdInt > 4 && boxIdInt <= 4 * 2) {
-            subCabinetNo = 2;
-            lockNo = boxIdInt - 4 * 1;
-        } else if (boxIdInt > 4 * 2 && boxIdInt <= 4 * 3) {
-            subCabinetNo = 3;
-            lockNo = boxIdInt - 4 * 2;
-        } else if (boxIdInt > 4 * 3 && boxIdInt <= 4 * 4) {
-            subCabinetNo = 4;
-            lockNo = boxIdInt - 4 * 3;
-        } else if (boxIdInt > 4 * 4 && boxIdInt <= 4 * 5) {
-            subCabinetNo = 5;
-            lockNo = boxIdInt - 4 * 4;
-        } else if (boxIdInt > 4 * 5 && boxIdInt <= 4 * 6) {
-            subCabinetNo = 6;
-            lockNo = boxIdInt - 4 * 5;
-        } else if (boxIdInt > 4 * 6 && boxIdInt <= 4 * 7) {
-            subCabinetNo = 7;
-            lockNo = boxIdInt - 4 * 6;
-        } else if (boxIdInt > 4 * 7 && boxIdInt <= 4 * 8) {
-            subCabinetNo = 8;
-            lockNo = boxIdInt - 4 * 7;
-        } else if (boxIdInt > 4 * 8 && boxIdInt <= 4 * 9) {
-            subCabinetNo = 9;
-            lockNo = boxIdInt - 4 * 8;
-        } else if (boxIdInt > 4 * 9 && boxIdInt <= 4 * 10) {
-            subCabinetNo = 10;
-            lockNo = boxIdInt - 4 * 9;
-        } else if (boxIdInt > 4 * 10 && boxIdInt <= 4 * 11) {
-            subCabinetNo = 11;
-            lockNo = boxIdInt - 4 * 10;
-        } else if (boxIdInt > 4 * 11 && boxIdInt <= 4 * 12) {
-            subCabinetNo = 12;
-            lockNo = boxIdInt - 4 * 11;
-        } else if (boxIdInt > 4 * 12 && boxIdInt <= 4 * 13) {
-            subCabinetNo = 13;
-            lockNo = boxIdInt - 4 * 12;
-        } else if (boxIdInt > 4 * 13 && boxIdInt <= 4 * 14) {
-            subCabinetNo = 14;
-            lockNo = boxIdInt - 4 * 13;
-        }
+
+        //适配无数个箱子 4*N
+        subCabinetNo = (int) Math.ceil(boxIdInt / 4f);
+        lockNo = boxIdInt - 4 * (subCabinetNo - 1);
+
+//<=14个子柜子，每个子柜4个箱子
+//        if (boxIdInt <= 4) {
+//            subCabinetNo = 1;
+//            lockNo = boxIdInt - 4 * 0;
+//        } else if (boxIdInt > 4 && boxIdInt <= 4 * 2) {
+//            subCabinetNo = 2;
+//            lockNo = boxIdInt - 4 * 1;
+//        } else if (boxIdInt > 4 * 2 && boxIdInt <= 4 * 3) {
+//            subCabinetNo = 3;
+//            lockNo = boxIdInt - 4 * 2;
+//        } else if (boxIdInt > 4 * 3 && boxIdInt <= 4 * 4) {
+//            subCabinetNo = 4;
+//            lockNo = boxIdInt - 4 * 3;
+//        } else if (boxIdInt > 4 * 4 && boxIdInt <= 4 * 5) {
+//            subCabinetNo = 5;
+//            lockNo = boxIdInt - 4 * 4;
+//        } else if (boxIdInt > 4 * 5 && boxIdInt <= 4 * 6) {
+//            subCabinetNo = 6;
+//            lockNo = boxIdInt - 4 * 5;
+//        } else if (boxIdInt > 4 * 6 && boxIdInt <= 4 * 7) {
+//            subCabinetNo = 7;
+//            lockNo = boxIdInt - 4 * 6;
+//        } else if (boxIdInt > 4 * 7 && boxIdInt <= 4 * 8) {
+//            subCabinetNo = 8;
+//            lockNo = boxIdInt - 4 * 7;
+//        } else if (boxIdInt > 4 * 8 && boxIdInt <= 4 * 9) {
+//            subCabinetNo = 9;
+//            lockNo = boxIdInt - 4 * 8;
+//        } else if (boxIdInt > 4 * 9 && boxIdInt <= 4 * 10) {
+//            subCabinetNo = 10;
+//            lockNo = boxIdInt - 4 * 9;
+//        } else if (boxIdInt > 4 * 10 && boxIdInt <= 4 * 11) {
+//            subCabinetNo = 11;
+//            lockNo = boxIdInt - 4 * 10;
+//        } else if (boxIdInt > 4 * 11 && boxIdInt <= 4 * 12) {
+//            subCabinetNo = 12;
+//            lockNo = boxIdInt - 4 * 11;
+//        } else if (boxIdInt > 4 * 12 && boxIdInt <= 4 * 13) {
+//            subCabinetNo = 13;
+//            lockNo = boxIdInt - 4 * 12;
+//        } else if (boxIdInt > 4 * 13 && boxIdInt <= 4 * 14) {
+//            subCabinetNo = 14;
+//            lockNo = boxIdInt - 4 * 13;
+//        }
 
         if (lockNo == 1 || lockNo == 2) {
             b[0] = (byte) ((subCabinetNo - 1) * 2 + 1);
